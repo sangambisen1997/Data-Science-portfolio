ad655f7bb4f1043fc9575c029ae0707f339abdf5
sangambisen1997 <sangambisen1997@gmail.com>
2022-08-04 00:37:11 +0530

    added utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+from flask import request
+
+
+def request_input():
+
+    data = request.get_json()
+    SepalWidth = data["SepalWidthCm"]
+    print("SepalWidth", SepalWidth)
+    PetalLength = data["PetalLengthCm"]
+    print("PetalLength", PetalLength)
+    PetalWidth = data["PetalWidthCm"]
+    print("PetalWidth", PetalWidth)
+    Species = data["Species"]
+    print("Species", Species)
+
+    return SepalWidth,PetalLength,PetalWidth,Species
+
+def user_details():
+    data = request.get_json()
+    name = data["name"]
+    address = data["address"]
+
+def login():
+    print("User name")
+
+    return name, address
